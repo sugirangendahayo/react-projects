51a5de6e809b48cc8f1ccb2cc779c627c21afd31
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-08 15:41:32 +0200

    Add components directory structure

---
+++ src/components/HelloWord.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWord = () => {
+  return (
+    <div className="bg-yellow-400  mx-auto h-screen flex justify-center items-center">
+      <p>Hello, world!</p>
+    </div>
+  );
+};
+
+export default HelloWord;
