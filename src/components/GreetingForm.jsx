a1fe3b4b69849de3b59fea0cd63e89ac88c627c7
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-08 17:45:35 +0200

    feat: add GreetingForm component to handle user name input and display alert

--- src/components/GreetingForm.jsx
+++ src/components/GreetingForm.jsx
@@ -1,16 +1,38 @@
 import React from "react";
 
 const GreetingForm = () => {
+  const [firstName, setFirstName] = React.useState("");
+  const [lastName, setLastName] = React.useState("");
+  function greetUser() {
+    alert(`Hello ${firstName} ${lastName}`);
+  }
   return (
     <div className="flex flex-col justify-center items-center gap-4">
       <div>
-        <input type="text" placeholder="First name" className="border border-gray-300 p-2 rounded"/>
+        <input
+          type="text"
+          placeholder="First name"
+          className="border border-gray-300 p-2 rounded"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
       </div>
       <div>
-        <input type="text" placeholder="Last name" className="border border-gray-300 p-2 rounded"/>
+        <input
+          type="text"
+          placeholder="Last name"
+          className="border border-gray-300 p-2 rounded"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </div>
       <div>
-        <button className="bg-red-600 text-white uppercase px-9 py-3 font-semibold rounded cursor-pointer">greet me</button>
+        <button
+          className="bg-red-600 text-white uppercase px-9 py-3 font-semibold rounded cursor-pointer"
+          onClick={greetUser}
+        >
+          greet me
+        </button>
       </div>
     </div>
   );
