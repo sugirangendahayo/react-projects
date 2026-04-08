8e72a82834aabb27e241eed53317864c02a9df59
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-08 17:31:36 +0200

    Add GreetingForm.jsx component

---
+++ src/components/GreetingForm.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const GreetingForm = () => {
+  return (
+    <div className="flex flex-col justify-center items-center gap-4">
+      <div>
+        <input type="text" placeholder="First name" className="border border-gray-300 p-2 rounded"/>
+      </div>
+      <div>
+        <input type="text" placeholder="Last name" className="border border-gray-300 p-2 rounded"/>
+      </div>
+      <div>
+        <button className="bg-red-600 text-white uppercase px-9 py-3">greet me</button>
+      </div>
+    </div>
+  );
+};
+
+export default GreetingForm;
