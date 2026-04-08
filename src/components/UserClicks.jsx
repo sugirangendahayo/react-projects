a2755b41f0e2029d1f5204850ccee4f7f486c432
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-08 17:30:56 +0200

    Update UserClicks.jsx component

--- src/components/UserClicks.jsx
+++ src/components/UserClicks.jsx
@@ -1,21 +1,9 @@
 import React from "react";
 
-const UserClicks = () => {
-  const exerciseLinks = [
-    "Home",
-    "Exercise 1",
-    "Exercise 2",
-    "Exercise 3",
-    "Exercise 4",
-    "Exercise 5",
-    "Exercise 6",
-    "Exercise 7",
-    "Exercise 8",
-    "Exercise 9",
-  ];
-  const handleClick = ()=>{
-    alert("Clicked!")
-  }
+const UserClicks = ({ exerciseLinks }) => {
+  function handleClick() {
+        alert("Clicked!");
+    }
   return (
     <div className="flex-col ">
       <ul className="flex justify-center items-center gap-4 py-6">
@@ -28,10 +16,13 @@ const UserClicks = () => {
         ))}
       </ul>
       <div className="flex justify-center items-center mt-[10%]">
-        
-      {/* <button className="bg-black text-white rounded cursor-pointer px-3 py-2" onClick={handleClick}>Click</button> */}
+        <button
+          className="bg-black text-white rounded cursor-pointer px-3 py-2"
+          onClick={handleClick}
+        >
+          Click
+        </button>
       </div>
-     
     </div>
   );
 };
