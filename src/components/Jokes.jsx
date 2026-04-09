34497898312b5b6cbbd40e2a4cbffbd8ce84205f
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-09 10:09:49 +0200

    feat: add Jokes component to display setup and punchline cards

--- src/components/Jokes.jsx
+++ src/components/Jokes.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const Jokes = ({jokes}) => {
+const Jokes = ({ jokes }) => {
   return (
     <>
-    {
-        jokes.map(joke=>(
-            <div key={joke.id} className="border border-gray-300 p-4 rounded">
-                <h2 className="text-xl font-semibold">{joke.setup}</h2>
-                <p className="text-gray-600">{joke.punchline}</p>
-            </div>
-        ))
-    }
+      <div className="flex justify-between items-center gap-4 bg-slate-100 p-4">
+        {jokes.map((joke) => (
+          <div
+            key={joke.id}
+            className="border border-gray-300 p-4 rounded cursor-pointer 
+          transition-all duration-200 ease-in-out 
+          hover:-translate-y-1 hover:shadow-lg bg-green-200"
+          >
+            <h2 className="text-xl font-semibold">{joke.setup}</h2>
+            <p className="text-gray-600">{joke.punchline}</p>
+          </div>
+        ))}
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Jokes
+export default Jokes;
