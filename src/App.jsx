ba57d6a73a9a6d2678264db2c5c6134eb2e7adf5
sugirangendahayo <sugirangendahayo835@gmail.com>
2026-04-23 16:27:22 +0200

    Update App.jsx to use useCalc hook instead of useCalculator

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,39 @@
-// import { useCalculator } from "./hooks/useCalculator";
-// import ButtonGrid from "./components/Main/ui/ButtonGrid";
-// import Display from "./components/Main/ui/Display";
-
-import Cart from "./components/Cart";
+import ButtonGrid from "./components/Main/ui/ButtonGrid";
+import Display from "./components/Main/ui/Display";
+import TouchTypingApp from "./components/TouchTypingApp";
+import TwoStepForm from "./components/TwoStepForm";
+import { useCalc } from "./hooks/useCalc";
 
 export default function App() {
-  // const { result, expression, handlePress } = useCalculator();
+  // const {
+  //   current,
+  //   previous,
+  //   operation,
+  //   addDigit,
+  //   chooseOperation,
+  //   calculate,
+  //   clear,
+  //   percentage,
+  //   toggleSign,
+  // } = useCalc();
   return (
-    /*  <div className="h-screen flex items-center justify-center">
-      <div className="bg-gray-800 rounded-3xl p-5 w-80 shadow-2xl ">
-        <Display result={result} expression={expression} />
-        <ButtonGrid onPress={handlePress} />
-      </div>
-    </div> */
-    <>
-      <Cart />
-    </>
+    <div
+    //  className="h-screen flex items-center justify-center"
+    >
+      {/* <div className="bg-gray-800 rounded-3xl p-5 w-80 shadow-2xl ">
+        <Display current={current} previous={previous} operation={operation} />
+        <ButtonGrid
+          current={current}
+          addDigit={addDigit}
+          chooseOperation={chooseOperation}
+          calculate={calculate}
+          clear={clear}
+          percentage={percentage}
+          toggleSign={toggleSign}
+        />
+      </div> */}
+      {/* <TouchTypingApp /> */}
+      <TwoStepForm />
+    </div>
   );
 }
